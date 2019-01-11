ebb8d09de5243f8b09ec4dd2510806126129023b
Yvonne Anyango <ivonnahbonswuh@gmail.com>
2019-01-11 17:23:34 +0300

    create neighbourhood model

--- Neighbours/models.py
+++ Neighbours/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Neighbourhood(models.Model):
+    name = models.CharField(max_lenth = 150)
+    location = models.CharField(max_length = 150)
+    occupants_count = models.CharField(max_length = 150)
+
+
+    
