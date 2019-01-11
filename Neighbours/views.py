e9777731ea9c32ca29d38bc3ab1c2a0de2e3857d
Yvonne Anyango <ivonnahbonswuh@gmail.com>
2019-01-11 16:48:09 +0300

    create authentification

---
+++ Neighbours/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+
+# Create your views here.
+
+@login_required(login_url='accounts/login/')
+def welcome(request):
+
+    return render(request, 'welcome.html')
