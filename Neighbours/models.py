acad378908a4f9637277ac7e0893d43ea8e85600
Yvonne Anyango <ivonnahbonswuh@gmail.com>
2019-01-13 23:29:05 +0300

    create user model

--- Neighbours/models.py
+++ Neighbours/models.py
@@ -8,4 +8,30 @@ class Neighbourhood(models.Model):
     occupants_count = models.CharField(max_length = 150)
 
 
+    # methods 
+
+    def create_neighbourhood(self):
+        self.create()
+
+    def delete_neighbourhood(self):
+        self.delete()
+
+    def update_neighbourhood(self):
+        self.update()
+
+    def __str__(self):
+        return self.location
+
+    @classmethod
+    def get_neighbourhood(cls, neighbourhood_id):
+        neighbourhood = Neighbourhood.objects.filter(name__location__icontains=neighbourhood_id)
+        return neighbourhood
+
+
+    class User(models.Model):
+        username = models.CharField(max_length = 150)
+        id = models.PrimaryKey(User, null=True, blank=True, on_delete=models.CASCADE)
+        Neighbourhood = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
+        email address = models.CharField(max_length = 150)
+
     
