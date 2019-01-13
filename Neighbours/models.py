de8f67efb746468125f676f775ebc142a3d08df0
Yvonne Anyango <ivonnahbonswuh@gmail.com>
2019-01-13 23:51:23 +0300

    create business model

--- Neighbours/models.py
+++ Neighbours/models.py
@@ -28,10 +28,36 @@ class Neighbourhood(models.Model):
         return neighbourhood
 
 
-    class User(models.Model):
-        username = models.CharField(max_length = 150)
-        id = models.PrimaryKey(User, null=True, blank=True, on_delete=models.CASCADE)
-        Neighbourhood = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
-        email address = models.CharField(max_length = 150)
+class User(models.Model):
+    username = models.CharField(max_length = 150)
+    id = models.PrimaryKey(User, null=True, blank=True, on_delete=models.CASCADE)
+    Neighbourhood = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
+    user_email_address = models.CharField(max_length = 150)
+
+    
+class Business(models.Model):
+    business_name = models.CharField(max_length =150)
+    User = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
+    Neighbourhood = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
+    business_email_address = models.CharField(max_length =150)
+
+    def __str__(self):
+        return self.business_name
+    
+    def create_business(self):
+        self.create()
+
+    def delete_business(self):
+        self.delete()
+
+    def update_business(self):
+        self.update()
+
+    @classmethod
+    def search_by_business_name(cls,search_term):
+        business = cls.objects.filter(business_name__icontains=search_term)
+        return business
+
+
 
     
