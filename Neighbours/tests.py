d0095439f3d03fec7db04624972e7ad56f1b9d06
Yvonne Anyango <ivonnahbonswuh@gmail.com>
2019-01-14 00:38:38 +0300

    create tests for profile

--- Neighbours/tests.py
+++ Neighbours/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
 
 # Create your tests here.
+class ProfileTestClass(TestCase): 
+
+    # Set up method
+    def setUp(self):
+
+        # Creating profile and saving it
+        self.Ivy= Profile(profile = 'photo')
+        self.Ivy.save_profile()
+
+    # Testing instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.pic,Image))
+
+
+    def tearDown(self):
+        Profile.objects.all().delete()
+        Image.objects.all().delete()
