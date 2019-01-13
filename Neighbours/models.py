c55a8db02116e85263f48c51f1ecb14132f6b45d
Yvonne Anyango <ivonnahbonswuh@gmail.com>
2019-01-14 01:06:24 +0300

    create profile model

--- Neighbours/models.py
+++ Neighbours/models.py
@@ -28,15 +28,44 @@ class Neighbourhood(models.Model):
         return neighbourhood
 
 
+class Profile(models.Model):
+    profile = models.ImageField(upload_to = 'profiles/')
+    username = models.CharField(max_length = 100)
+    bio = models.CharField(max_length = 500)
+    Flashes = models.ForeignKey(User, on_delete=models.CASCADE)
+    contacts = models.CharField(max_length = 50)
+
+
+    # Methods
+    def __str__(self):
+        return self.username
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    def update_profile(self):
+        self.update()
+
+    
+
+    @classmethod
+    def get_profile(cls, profile_id):
+        profile = Profile.objects.filter(name__username__icontains=profile_id)
+        return profile
+
+
 class User(models.Model):
     username = models.CharField(max_length = 150)
-    id = models.PrimaryKey(User, null=True, blank=True, on_delete=models.CASCADE)
+    id = models.PrimaryKey(id)
     Neighbourhood = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
     user_email_address = models.CharField(max_length = 150)
 
     
 class Business(models.Model):
-    business_id = models.PrimaryKey
+    business_id = models.PrimaryKey(business_id)
     business_name = models.CharField(max_length =150)
     User = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
     Neighbourhood = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
